581e122d9d4b37c6a8a1c99f36f7a8de896485d6
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2020-04-29 07:57:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,64 @@ terraform {
   }
 }
 
-resource "random_pet" "name" {
 
-  length    = "5"
-  separator = "-"
+  
+variable "region" {
+  description = "AWS default region"
+  default     = "us-east-2"
 }
 
-output "out" {
-  value = "${random_pet.name.id}"
+variable "ami" {
+  description = "AWS ami according to the region"
+  default     = "ami-056569351c9d4c8cf"
+}
+
+variable "instance_type" {
+  description = "AWS instance characteristics"
+  default     = "t2.micro"
+}
+
+variable "aws_security_group_name" {
+  description = "Dedicated security group"
+  default     = "windiws-test"
+}
+
+variable "ingress_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [3389, 5985, 5986]
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "windows" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.windows_sg.id]
+}
+
+# Example of for_each function use
+resource "aws_security_group" "windows_sg" {
+  name        = var.aws_security_group_name
+  description = "Security group settings"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress_ports
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
